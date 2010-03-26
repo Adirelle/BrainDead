8428615e84931a97c7c882b9726a979d874ec524
Adirelle <adirelle@tagada-team.net>
2010-03-26 11:46:50 +0100

    SellJunk also creates a chat command to destroy junk items.

--- modules/SellJunk.lua
+++ modules/SellJunk.lua
@@ -6,16 +6,25 @@ All rights reserved.
 
 local _, addon = ...
 
-local mod = addon:NewModule('SellJunk')
+local mod = addon:NewModule('SellJunk', 'AceConsole-3.0')
 
-function mod:OnEnable()
-	self:Debug('Enabled')
-	self:RegisterEvent('MERCHANT_SHOW', 'Sell')
+function mod:OnInitialize()
+	self:RegisterChatCommand('destroyjunk', 'Destroy', true)
+	self:RegisterChatCommand('dj', 'Destroy', true)
+	self:Debug('Initialized')
 end
 
-function mod:Sell()
-	if GetCursorInfo() then return end
-	self:Debug('Sell')
+function mod:OnEnable()
+	self:RegisterEvent('MERCHANT_SHOW', 'Sell')
+	self:Debug('Enabled')
+end
+
+function mod:Process(callback, action, report, noItems)
+	if GetCursorInfo() then 
+		return self:Feedback('Cannot '..action..' items while another action is pending.')
+	end
+	self:Debug('Looking for junk to', action)
+	local func = self[callback]
 	local count, money = 0, 0
 	for bag = 0, NUM_BAG_SLOTS do
 		for slot = 1, GetContainerNumSlots(bag) do
@@ -23,13 +32,12 @@ function mod:Sell()
 			if texture and link and quality and not locked then
 				local linkColor = link:match('(|cff[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9])')
 				if (quality == ITEM_QUALITY_POOR or linkColor == ITEM_QUALITY_COLORS[ITEM_QUALITY_POOR].hex) and link:match('item:%d+:0:0:0:0') then
-					self:Debug('bag,slot:', bag, slot, 'link:', link, 'quality:', quality, 'linkColor:', linkColor..'XXXX|r')
-					UseContainerItem(bag, slot)
 					if GetCursorInfo() then
-						self:Debug('Could not sell', link)
-						-- Something didn't worked, put it back
-						PickupContainerItem(bag, slot)
-					else
+						self:Feedback('Some weird error has happened; aborting to prevent further failures.')
+						break
+					end
+					self:Debug('bag,slot:', bag, slot, 'link:', link, 'quality:', quality, 'linkColor:', linkColor..'XXXX|r')
+					if callback(self, bag, slot) then
 						money = money + (tonumber(select(11, GetItemInfo(link))) or 0)
 						count = count + 1
 					end
@@ -38,6 +46,41 @@ function mod:Sell()
 		end
 	end
 	if count > 0 then
-		self:Feedback('Sold', count, 'items for', GetCoinTextureString(money))
+		self:Feedback(report:format(count, GetCoinTextureString(money)))
+	elseif noItems then
+		self:Feedback(noItems)
 	end
+end
+
+function mod:SellItem(bag, slot, link)
+	UseContainerItem(bag, slot)
+	if GetCursorInfo() then
+		self:Debug('Could not sell item', bag, slot, link, 'GetCursorInfo:', GetCursorInfo())
+		PickupContainerItem(bag, slot)
+		return
+	end
+	return true
+end
+
+function mod:DestroyItem(bag, slot, link)
+	PickupContainerItem(bag, slot)
+	if not CursorHasItem() or select(3, GetCursorInfo()) ~= link then
+		self:Debug('Could not pickup item', bag, slot, link, 'GetCursorInfo:', GetCursorInfo())
+		return
+	end
+	DeleteCursorItem()
+	if GetCursorInfo() then
+		self:Debug('Could not destroy item', bag, slot, link, 'GetCursorInfo:', GetCursorInfo())
+		PickupContainerItem(bag, slot)
+		return
+	end
+	return true
+end
+
+function mod:Sell()
+	return self:Process('SellItem', 'sell', 'Sold %d items for %s.')
+end
+
+function mod:Destroy()
+	return self:Process('DestroyItem', 'destroy', 'Destroyed %d items, loss: %s.', 'No junk to destroy.')
 end
