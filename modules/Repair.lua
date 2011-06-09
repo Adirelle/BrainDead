5d8d0c9f57cb2825710c69a0a69bc1897d1d9058
Adirelle <adirelle@tagada-team.net>
2011-06-10 00:11:00 +0200

    The Repair module now automatically uses guild repair when possible.

--- modules/Repair.lua
+++ modules/Repair.lua
@@ -14,19 +14,32 @@ function mod:OnEnable()
 end
 
 function mod:Repair()
-	if CanMerchantRepair() then
-		local cost, canRepair = GetRepairAllCost()
-		if canRepair and type(cost) == "number" then
-			if cost / GetMoney() < 0.5 then
-				self:Feedback('Repairing all items for', GetCoinTextureString(cost))
-				RepairAllItems()
-			else
-				self:Feedback('Not enough money to repair ; repair cost:', GetCoinTextureString(cost))
-			end
+	local cost, canRepair = GetRepairAllCost()
+	if type(cost) ~= "number" or not canRepair then
+		self:Debug("Cannot or no need to repair", cost, canRepair)
+		return
+	end
+	if CanGuildBankRepair() then
+		local amount, total = GetGuildBankWithdrawMoney(), GetGuildBankMoney()
+		if amount == -1 then
+			amount = total
 		else
-			self:Debug("Can't or no need to repair", cost, canRepair)
+			amount = min(amount, total)
+		end
+		if amount >= cost then
+			self:Feedback('Repairing all items using guild money for', GetCoinTextureString(cost))
+			RepairAllItems(true)
+			return
+		end
+	end
+	if CanMerchantRepair() then
+		if 2 * cost <= GetMoney() then
+			self:Feedback('Repairing all items for', GetCoinTextureString(cost))
+			RepairAllItems()
+		else
+			self:Feedback('Not enough money to repair; cost:', GetCoinTextureString(cost))
 		end
 	else
-		self:Debug("Merchant can't repair")
+		self:Debug("This merchant cannot repair")
 	end
 end
