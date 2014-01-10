368ee4ef0210985d237a918b9a2f41c1a1dc12a3
Adirelle <adirelle@gmail.com>
2014-01-10 16:43:08 +0100

    AcceptInvitation: fixed for 5.x.

--- modules/AcceptInvitation.lua
+++ modules/AcceptInvitation.lua
@@ -13,34 +13,55 @@ function mod:OnEnable()
 	self:RegisterEvent('PARTY_INVITE_REQUEST')
 end
 
-function mod:PARTY_INVITE_REQUEST(event, requestor)
-	local accept = UnitIsInMyGuild(requestor) and 'guildmate'
-	if not accept then
-		for i = 1, GetNumFriends() do
-			if requestor == GetFriendInfo(i) then
-				accept = 'friend'
-				break
+local function DoIKnowYou(name)
+	for i = 1, GetNumFriends() do
+		if requestor == GetFriendInfo(i) then
+			return 'friend'
+		end
+	end
+	if IsInGuild() then
+		local shortname, realm = strsplit('-', name, 2)
+		local fqdn = shortname..'-'..(realm or GetRealmName())
+		for i = 1, GetNumGuildMembers() do
+			if fqdn == GetGuildRosterInfo(i) then
+				return 'guildmate'
 			end
 		end
 	end
+end
+
+function mod:PARTY_INVITE_REQUEST(event, requestor)
+	local accept = DoIKnowYou(requestor)
+	self:Debug('Invitation from', requestor, '=>', accept)
 	if accept then
+		self:Debug('Accepting invitation from', accept, requestor)
 		if not self:AssertNotDND('Not accepting invitation while DND') then
+			self:Debug('DND denial')
 			return
 		end
-		local lfgMode = GetLFGMode()
+		local lfgMode = GetLFGMode(LE_LFG_CATEGORY_LFD)
+			or GetLFGMode(LE_LFG_CATEGORY_RF)
+			or GetLFGMode(LE_LFG_CATEGORY_SCENARIO)
+			or GetLFGMode(LE_LFG_CATEGORY_LFR)
+		self:Debug('lfgMode', lfgMode)
 		if lfgMode and lfgMode ~= "abandonedInDungeon" then
 			self:Feedback("Not accepting while invitation while in LFG queue")
 			return
 		end
 		self:Feedback('Accepting invitation from', accept, requestor)
-		AcceptGroup()
 		self.requestor = requestor
-		self:RegisterEvent('PARTY_MEMBERS_CHANGED')
+		self:RegisterEvent('GROUP_ROSTER_UPDATE')
+		AcceptGroup()
+	else
+		self:Debug('Invitation not automatically accepted')
 	end
 end
 
-function mod:PARTY_MEMBERS_CHANGED()
-	self:UnregisterEvent('PARTY_MEMBERS_CHANGED')
+function mod:GROUP_ROSTER_UPDATE()
+	self:UnregisterEvent('GROUP_ROSTER_UPDATE')
 	StaticPopup_Hide('PARTY_INVITE')
-	self:AFKWarning(self.requestor, "Automatically accepted invitation.")
+	if self.requestor then
+		self:AFKWarning(self.requestor, "Automatically accepted invitation.")
+		self.requestor = nil
+	end
 end
